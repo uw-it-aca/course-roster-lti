df0bd3f1db8979b84c069cdaec8fd937ad432f8f
Jim Laney <jlaney@uw.edu>
2017-01-27 16:08:31 -0800

    drops handlebars

--- travis-ci/settings.py
+++ travis-ci/settings.py
@@ -35,7 +35,6 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.humanize',
-    'templatetag_handlebars',
     'restclients',
     'userservice',
     'sis_provisioner',

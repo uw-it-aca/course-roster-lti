5693e77b475e610b4dad2b2bd97e7d2e543df61a
Jim Laney <jlaney@uw.edu>
2017-05-15 09:22:29 -0700

    python3 compatible urls

--- course_roster/urls.py
+++ course_roster/urls.py
@@ -1,14 +1,13 @@
-from django.conf.urls import patterns, url, include
+from django.conf.urls import url
 from course_roster.views import (
     LaunchView, CourseRoster, CourseSections, RosterPhoto)
 
 
-urlpatterns = patterns(
-    '',
+urlpatterns = [
     url(r'^$', LaunchView.as_view()),
     url(r'^photos/(?P<photo_key>[a-z0-9]*)$', RosterPhoto.as_view()),
     url(r'^api/v1/course/(?P<canvas_course_id>[0-9]+)/people$',
         CourseRoster().run),
     url(r'^api/v1/course/(?P<canvas_course_id>[0-9]+)/sections$',
         CourseSections().run),
-)
+]

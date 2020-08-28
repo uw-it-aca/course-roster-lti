69c15e36553b834aba6e817ba3c8c3dd80d1a1e5
Jim Laney <jlaney@uw.edu>
2020-08-28 10:38:04 -0700

    adds docker config for course_roster app

---
+++ docker/settings.py
@@ -0,0 +1,17 @@
+from .base_settings import *
+
+DEBUG = True if os.getenv('ENV', 'localdev') == 'localdev' else False
+
+INSTALLED_APPS += [
+    'course_roster.apps.CourseRosterConfig',
+    'compressor',
+]
+
+COMPRESS_ROOT = "/static/"
+COMPRESS_PRECOMPILERS = (("text/less", "lessc {infile} {outfile}"),)
+COMPRESS_OFFLINE = True
+STATICFILES_FINDERS += ("compressor.finders.CompressorFinder",)
+
+COURSE_ROSTER_PER_PAGE = 50
+
+LOGGING = {}

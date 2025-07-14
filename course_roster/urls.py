e9c0b26c4594256f614f086f3eb19b29658fbe21
mike seibel <mikes@washington.edu>
2025-07-14 14:03:52 -0700

    initial lti 1.3 config

--- course_roster/urls.py
+++ course_roster/urls.py
@@ -6,7 +6,7 @@ from course_roster.views import (
     LaunchView, CourseRoster, CourseSections, RosterPhoto)
 
 urlpatterns = [
-    re_path(r'^$', LaunchView.as_view()),
+    re_path(r'^$', LaunchView.as_view(), name="lti-launch"),
     re_path(r'^photos/(?P<photo_key>[a-z0-9]*)$',
             RosterPhoto.as_view(), name='photo'),
     re_path(r'^api/v1/course/(?P<canvas_course_id>[0-9]+)/people$',

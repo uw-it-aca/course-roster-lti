af9f816b80cf7c3952f86d2b5351578e0d52fb1a
Jim Laney <jlaney@uw.edu>
2026-09-02 09:11:54 -0700

    ruff fixes

--- course_roster/tests.py
+++ course_roster/tests.py
@@ -1,24 +1,26 @@
 # Copyright 2026 UW-IT, University of Washington
 # SPDX-License-Identifier: Apache-2.0
 
+import re
+from unittest import mock
+
 from django.test import TestCase
+
 from course_roster.dao.canvas import *
 from course_roster.dao.idcard import *
-import mock
-import re
 
 
 class CanvasDAOTest(TestCase):
     @mock.patch('course_roster.dao.canvas.Users')
     def test_users_constructor(self, mock_object):
-        r = get_users_for_course('123', '456', '10')
+        _r = get_users_for_course('123', '456', '10')
         mock_object.assert_called_with(as_user='456')
 
     @mock.patch.object(Users, 'next_page_url', create=True)
     @mock.patch.object(Users, 'get_users_for_course')
     def test_get_course_users(self, mock_method, mock_attribute):
         with self.settings(COURSE_ROSTER_PER_PAGE=1000):
-            r = get_users_for_course('123', '456', '10')
+            _r = get_users_for_course('123', '456', '10')
             mock_method.assert_called_with('123', params={
                 'page': '10',
                 'per_page': 1000,
@@ -28,17 +30,17 @@ class CanvasDAOTest(TestCase):
 
     @mock.patch.object(Enrollments, 'get_enrollments_for_course')
     def test_get_enrollments_for_course(self, mock_method):
-        r = get_enrollments_for_course('123', '456')
+        _r = get_enrollments_for_course('123', '456')
         mock_method.assert_called_with('123', params={'user_id': '456'})
 
     @mock.patch('course_roster.dao.canvas.Sections')
     def test_sections_constructor(self, mock_object):
-        r = get_sections_in_course('123', '456')
+        _r = get_sections_in_course('123', '456')
         mock_object.assert_called_with(as_user='456')
 
     @mock.patch.object(Sections, 'get_sections_in_course')
     def test_get_sections_in_course(self, mock_method):
-        r = get_sections_in_course('123', '456')
+        _r = get_sections_in_course('123', '456')
         mock_method.assert_called_with('123')
 
     def test_get_viewable_sections(self):
@@ -56,7 +58,7 @@ class IDPhotoTest(TestCase):
         result = re.search(r'/([a-z0-9]{16})$', url)
         url_key = result.group(1)
 
-        r = get_photo(url_key)
+        _r = get_photo(url_key)
         mock_method.assert_called_with(reg_id, size=image_size)
 
         # Key no longer exists
